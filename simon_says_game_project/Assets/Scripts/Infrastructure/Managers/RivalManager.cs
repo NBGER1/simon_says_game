f0d9a4caaf3c43a6b30e0979d3710e52d2b242ae
Naor <naorberger@gmail.com>
2021-09-25 13:57:07 +0300

    dynamic loading

--- simon_says_game_project/Assets/Scripts/Infrastructure/Managers/RivalManager.cs
+++ simon_says_game_project/Assets/Scripts/Infrastructure/Managers/RivalManager.cs
@@ -1,30 +1,41 @@
-using Gameplay.Core;
+using System;
+using System.Linq;
 using Gameplay.Rivals;
 using Infrastructure.Abstracts;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Infrastructure.Managers
 {
     public class RivalManager : Singleton<RivalManager>
     {
-        #region Editor
+        #region Fields
 
-        [SerializeField] private RivalModel[] _rivals;
+        private RivalModel[] _rivals;
 
         #endregion
 
 
         #region Methods
 
+        private void Start()
+        {
+            DontDestroyOnLoad(gameObject);
+            if (_rivals?.Length >= 1) return;
+            _rivals = Resources.LoadAll("RivalModels", typeof(RivalModel)).Cast<RivalModel>().ToArray();
+        }
+
         protected override RivalManager GetInstance()
         {
             return this;
         }
 
-        public (RivalModel,int) GetRandomRival()
+        public (RivalModel, int) GetRandomRival()
         {
             var randomIndex = Random.Range(0, _rivals.Length);
-            return (_rivals[randomIndex],randomIndex);
+            Debug.Log($"RandomIndex = {randomIndex} , ArrayLength {_rivals.Length}");
+
+            return (_rivals[randomIndex], randomIndex);
         }
 
         public RivalModel GetRivalByIndex(int index)
